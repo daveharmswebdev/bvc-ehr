f51521f8a7336bcaa93d07c0e69a012d88905dd5
Dave Harms <daveharmswebdev@gmail.com>
2016-10-21 18:08:37 -0500

    made test for post /api/broset

--- routes/broset.js
+++ routes/broset.js
@@ -9,8 +9,35 @@ router.get('/api/broset', (req, res, next) => {
 	knex('broset')
 		.select()
 		.then(broset => {
-			console.log('broset', broset)
 			res.status(200).json(broset)
+		})
+		.catch( error => next(error))
+})
+
+router.post('/api/broset', (req, res, next) => {
+	console.log('req.body', req.body)
+	knex('broset')
+		.insert({
+			broset_id:4,
+			admission_id: 1,
+			user_id: 1,
+			confused: true,
+			irritable: true,
+			boisterous: true,
+			verbal_threats: true,
+			physical_threats: true,
+			attacking_furniture: true
+		})
+		.then( id => {
+			knex('broset')
+				.select()
+				.where({broset_id:id[0]})
+				.then( broset => {
+					res.status(200).json(broset)
+				})
+		})
+		.catch(function(error) {
+			next(error)
 		})
 })
 
