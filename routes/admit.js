4c699b3989695dc407e92c2a72b546b3763ef677
Dave Harms <daveharmswebdev@gmail.com>
2016-10-23 16:07:52 -0500

    tdd get by id and post for /api/admit

--- routes/admit.js
+++ routes/admit.js
@@ -15,20 +15,32 @@ router.get('/api/admit', (req, res, next) => {
 		.catch( error => next(error))
 })
 
-// router.post('/api/admit', (req, res, next) => {
-// 	knex('admission')
-// 		.insert(req.body)
-// 		.returning('broset_id')
-// 		.then( id => {
-// 			knex('admission')
-// 				.select()
-// 				.where('broset_id', id[0])
-// 				.then( score => {
-// 					res.status(200).json(score[0])
-// 				})
-// 		})
-// 		.catch( error => next(error))
-// })
+// returns admission based on id
+router.get('/api/admit/:id', (req, res, next) => {
+	knex('admission')
+		.select()
+		.where('admission_id', req.params.id)
+		.then(admissions => {
+			res.status(200).json(admissions)
+		})
+		.catch( error => next(error))
+})
+
+
+router.post('/api/admit', (req, res, next) => {
+	knex('admission')
+		.insert(req.body)
+		.returning('admission_id')
+		.then( id => {
+			knex('admission')
+				.select()
+				.where('admission_id', id[0])
+				.then( admission => {
+					res.status(200).json(admission[0])
+				})
+		})
+		.catch( error => next(error))
+})
 
 // router.put('/api/admit', (req, res, next) => {
 // 	knex('admission')
--- test/admission.spec.js
+++ test/admission.spec.js
@@ -53,57 +53,74 @@ describe('admit routes', () => {
 			})
 	})
 
-	// it('should post a assessment', done => {
-	// 	chai
-	// 		.request(app)
-	// 		.post('/api/admit')
-	// 		.send({
-	// 			"assessment_id":"4",
-	// 			"admission_id":"1",
-	// 			"oriented_person":"false",
-	// 			"oriented_place":"false",
-	// 			"oriented_time":"false",
-	// 			"oriented_purpose":"false",
-	// 			"suicidal":"true",
-	// 			"suicidal_plan":"gun",
-	// 			"homicidal":"true",
-	// 			"homicidal_plan":"kill neighbor",
-	// 			"visual_hallucinations":"sees dead brother",
-	// 			"audio_hallucinations":"hears dead brother",
-	// 			"tactile_hallucinations":"spider sensation",
-	// 			"hallucination_comments":"none",
-	// 			"affect":"flat",
-	// 			"appetite":"anorexic",
-	// 			"appearance":"gaunt",
-	// 			"speech":"slow",
-	// 			"nurse_assessing":"1",
-	// 		})
-	// 		.end((err, res) => {
-	// 			res.should.have.status(200)
-	// 			res.should.be.json // jshint ignore:line
-	// 			res.body.should.be.a('object')
-	// 			res.body.assessment_id.should.equal(4)
-	// 			res.body.admission_id.should.equal(1)
-	// 			res.body.oriented_person.should.equal(false)
-	// 			res.body.oriented_place.should.equal(false)
-	// 			res.body.oriented_time.should.equal(false)
-	// 			res.body.oriented_purpose.should.equal(false)
-	// 			res.body.suicidal.should.equal(true)
-	// 			res.body.suicidal_plan.should.equal('gun')
-	// 			res.body.homicidal.should.equal(true)
-	// 			res.body.homicidal_plan.should.equal('kill neighbor')
-	// 			res.body.visual_hallucinations.should.equal('sees dead brother')
-	// 			res.body.audio_hallucinations.should.equal('hears dead brother')
-	// 			res.body.tactile_hallucinations.should.equal('spider sensation')
-	// 			res.body.hallucination_comments.should.equal('none')
-	// 			res.body.affect.should.equal('flat')
-	// 			res.body.appetite.should.equal('anorexic')
-	// 			res.body.appearance.should.equal('gaunt')
-	// 			res.body.speech.should.equal('slow')
-	// 			res.body.nurse_assessing.should.equal(1)
-	// 			done()
-	// 		})
-	// })
+	it('should return admission based on id', (done) => {
+		chai
+			.request(app)
+			.get('/api/admit/1')
+			.end((err, res) => {
+				res.should.have.status(200)
+				res.should.be.json // jshint ignore:line
+				res.body.should.be.a('array')
+				res.body.length.should.equal(1)
+				res.body[0].admission_id.should.equal(1)
+				res.body[0].patient_id.should.equal(1)
+				res.body[0].admission_rn.should.equal(1)
+				res.body[0].voluntary_status.should.equal('voluntary')
+				res.body[0].complaint.should.equal('suicidal ideation')
+				res.body[0].symptoms.should.equal('depression crying insomnia')
+				res.body[0].suicidal.should.equal(true)
+				res.body[0].suicidal_plan.should.equal('intentional overdose of narcotic pain medication')
+				res.body[0].homicidal.should.equal(false)
+				expect(res.body[0].homicidal_who).to.be.null // jshint ignore:line
+				expect(res.body[0].homicidal_plan).to.be.undefined // jshint ignore:line
+				res.body[0].behavioral_health_hx.should.equal('depressed since age 10, one past attempt at age 26.')
+				res.body[0].medical_hx.should.equal('type 1 diabetes')
+				res.body[0].current_meds.should.equal('prozac insulin')
+				res.body[0].smoker.should.equal(false)
+				done()
+			})
+	})
+
+	it('should post a assessment', done => {
+		chai
+			.request(app)
+			.post('/api/admit')
+			.send({
+				"admission_id":"2",
+				"patient_id":"1",
+				"admission_rn":"1",
+				"voluntary_status":"voluntary-dpoa",
+				"complaint":"combative behavior secondary to alzheimers",
+				"symptoms":"a,b,c",
+				"suicidal":"false",
+				"homicidal":"false",
+				"behavioral_health_hx":"alzheimers",
+				"medical_hx":"CAD CABG",
+				"current_meds":"aspirin",
+				"smoker":"false"
+			})
+			.end((err, res) => {
+				res.should.have.status(200)
+				res.should.be.json // jshint ignore:line
+				res.body.should.be.a('object')
+				res.body.admission_id.should.equal(2)
+				res.body.patient_id.should.equal(1)
+				res.body.admission_rn.should.equal(1)
+				res.body.voluntary_status.should.equal('voluntary-dpoa')
+				res.body.complaint.should.equal('combative behavior secondary to alzheimers')
+				res.body.symptoms.should.equal('a,b,c')
+				res.body.suicidal.should.equal(false)
+				expect(res.body.suicidal_plan).to.be.null // jshint ignore:line
+				res.body.homicidal.should.equal(false)
+				expect(res.body.homicidal_who).to.be.null // jshint ignore:line
+				expect(res.body.homicidal_plan).to.be.undefined // jshint ignore:line
+				res.body.behavioral_health_hx.should.equal('alzheimers')
+				res.body.medical_hx.should.equal('CAD CABG')
+				res.body.current_meds.should.equal('aspirin')
+				res.body.smoker.should.equal(false)
+				done()
+			})
+	})
 
 	// it('should be able to updated an assessment', done => {
 	// 	chai
