4cad82b67e1e300672b5f1a15cea43d0e8591091
Dave Harms <daveharmswebdev@gmail.com>
2016-10-24 18:43:05 -0500

    tdd DELETE /api/medication/:1, get /api/medication get /api/medication/:id, get /api/medByIntervention/:id

--- test/medication.spec.js
+++ test/medication.spec.js
@@ -31,11 +31,49 @@ describe('medication routes', () => {
     });
   });
 
-	it('should return all medications for one admission_id', done => {
+	it('should return all medications', done => {
 		chai
 			.request(app)
 			.get('/api/medication')
-			.send({"intervention_id":"1"})
+			.end((err, res) => {
+				res.should.have.status(200)
+				res.should.be.json // jshint ignore:line
+				res.body.should.be.a('array')
+				res.body.length.should.equal(1)
+				res.body[0].medication_id.should.equal(1)
+				res.body[0].intervention_id.should.equal(1)
+				res.body[0].user_id.should.equal(1)
+				res.body[0].medication.should.equal('ativan')
+				res.body[0].dose.should.equal(2)
+				res.body[0].units.should.equal('mg')
+				res.body[0].route.should.equal('sub-cutaneous')
+				done()
+			})
+	})
+
+	it('should return all medications by medication_id', done => {
+		chai
+			.request(app)
+			.get('/api/medication/1')
+			.end((err, res) => {
+				res.should.have.status(200)
+				res.should.be.json // jshint ignore:line
+				res.body.should.be.a('object')
+				res.body.medication_id.should.equal(1)
+				res.body.intervention_id.should.equal(1)
+				res.body.user_id.should.equal(1)
+				res.body.medication.should.equal('ativan')
+				res.body.dose.should.equal(2)
+				res.body.units.should.equal('mg')
+				res.body.route.should.equal('sub-cutaneous')
+				done()
+			})
+	})
+
+	it('should return all medications for one intervention_id', done => {
+		chai
+			.request(app)
+			.get('/api/medByIntervention/1')
 			.end((err, res) => {
 				res.should.have.status(200)
 				res.should.be.json // jshint ignore:line
@@ -108,5 +146,26 @@ describe('medication routes', () => {
 				done()
 			})	
 	})
+
+  it('should be able to del medication by id', done => {
+    chai
+      .request(app)
+      .delete('/api/medication/1')
+      .end((err, res) => {
+        res.should.have.status(200)
+        res.should.be.json // jshint ignore:line
+        res.body.should.equal(1)    
+        chai
+          .request(app)
+          .get('/api/medication')
+          .end((err,res) => {
+            res.should.have.status(200)
+            res.should.be.json // jshint ignore:line
+            res.body.should.be.a('array')
+            res.body.length.should.equal(0)
+            done()            
+          })  
+      })
+  })   
 
 })
