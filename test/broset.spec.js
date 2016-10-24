1675b02e26f9e5044f43c9479805e542577a820a
Dave Harms <daveharmswebdev@gmail.com>
2016-10-24 18:03:05 -0500

    tdd DELETE /api/broset/:1, get /api/broset, get /api/brosetByAdmission/:1, get /api/broset/:id

--- test/broset.spec.js
+++ test/broset.spec.js
@@ -31,11 +31,53 @@ describe('broset routes', () => {
     });
   });
 
-	it('should return all scores based on admission', (done) => {
+  it('should return all scores', done => {
 		chai
 			.request(app)
 			.get('/api/broset')
-			.send({"admission_id":"1"})
+			.end((err, res) => {
+				res.should.have.status(200)
+				res.should.be.json // jshint ignore:line
+				res.body.should.be.a('array')
+				res.body.length.should.equal(3)
+				res.body[0].should.have.property('broset_id')
+				res.body[0].should.have.property('admission_id')
+				res.body[0].should.have.property('user_id')
+				res.body[0].should.have.property('confused')
+				res.body[0].should.have.property('irritable')
+				res.body[0].should.have.property('boisterous')
+				res.body[0].should.have.property('verbal_threats')
+				res.body[0].should.have.property('physical_threats')
+				res.body[0].should.have.property('attacking_furniture')
+				done()
+			})  	
+  })
+
+  it('should return broset by broset_id', done => {
+		chai
+			.request(app)
+			.get('/api/broset/1')
+			.end((err, res) => {
+				res.should.have.status(200)
+				res.should.be.json // jshint ignore:line
+				res.body.should.be.a('object')
+				res.body.broset_id.should.equal(1)
+				res.body.admission_id.should.equal(1)
+				res.body.user_id.should.equal(1)
+				res.body.confused.should.equal(true)
+				res.body.irritable.should.equal(true)
+				res.body.boisterous.should.equal(true)
+				res.body.verbal_threats.should.equal(false)
+				res.body.physical_threats.should.equal(true)
+				res.body.attacking_furniture.should.equal(false)
+				done()
+			})  	
+  })
+
+	it('should return all scores based on admission', (done) => {
+		chai
+			.request(app)
+			.get('/api/brosetByAdmission/1')
 			.end((err, res) => {
 				res.should.have.status(200)
 				res.should.be.json // jshint ignore:line
@@ -110,6 +152,28 @@ describe('broset routes', () => {
 				done()					
 			})	
 	})
+
+	it('should be able to del broset by id', done => {
+		chai
+			.request(app)
+			.delete('/api/broset/2')
+			.end((err, res) => {
+				res.should.have.status(200)
+				res.should.be.json // jshint ignore:line
+				res.body.should.equal(1)		
+				chai
+					.request(app)
+					.get('/api/broset')
+					.end((err,res) => {
+						res.should.have.status(200)
+						res.should.be.json // jshint ignore:line
+						res.body.should.be.a('array')
+						res.body.length.should.equal(2)
+						done()						
+					})	
+			})
 	})
+
+})
 
 
