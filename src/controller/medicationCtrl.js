f2130fbee8096828e59d7bb7f60775fbaa9e3f61
Dave Harms <daveharmswebdev@gmail.com>
2016-10-29 09:12:30 -0500

    created medicationData.js factory, created get and create methods for meds, client now able to display meds on medication entry view

--- src/controller/medicationCtrl.js
+++ src/controller/medicationCtrl.js
@@ -1,6 +1,11 @@
 'use strict'
 
-app.controller('MedicationCtrl', function($scope, $routeParams) {
+app.controller('MedicationCtrl', function($scope, $routeParams, MedicationData) {
+	const displayMeds = () => 
+		MedicationData
+			.getMedByInt($routeParams.interventionId)
+			.then(meds => $scope.medications = meds)
+
 	$scope.med = () => {
 		let medication = {
 			intervention_id: $routeParams.interventionId,
@@ -12,4 +17,6 @@ app.controller('MedicationCtrl', function($scope, $routeParams) {
 
 		console.log('medication', medication)
 	}
+
+	displayMeds()
 })
---
+++ src/factories/medicationData.js
@@ -0,0 +1,25 @@
+'use strict'
+
+app.factory('MedicationData', ($q, $http) => {
+	const service = {}
+
+	service.getMedByInt = intervention => {
+		return $q((resolve, reject) => {
+			$http
+				.get(`/api/medByIntervention/${intervention}`)
+				.success(meds => resolve(meds))
+				.error(error => reject(error))
+		})
+	}
+
+	service.createMed = submission => {
+		return $q((resolve, reject) => {
+			$http
+				.post('/api/medication', submission)
+				.success(response => resolve(response))
+				.error(error => reject(error))
+		})
+	}
+
+	return service
+})
