e3f06748192950c203593024ae4c516ef83315a1
Dave Harms <daveharmswebdev@gmail.com>
2016-10-30 21:12:30 -0500

    added seed files

---
+++ seeds/c1_admission.js
@@ -0,0 +1,28 @@
+'use strict'
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('admission').del()
+    .then(function () {
+      return Promise.all([
+        // Inserts seed entries
+        knex('admission')
+          .insert({
+            patient_id: 1,
+            admission_rn: 1,
+            voluntary_status: 'voluntary',
+            complaint: 'suicidal ideation',
+            symptoms: 'depression crying insomnia',
+            suicidal: true,
+            suicidal_plan: 'intentional overdose of narcotic pain medication',
+            homicidal: false,
+            behavioral_health_hx: 'depressed since age 10, one past attempt at age 26.',
+            medical_hx: 'type 1 diabetes',
+            current_meds: 'prozac insulin',
+            smoker: false,
+            room: '202'
+          }),
+
+      ]);
+    });
+};
